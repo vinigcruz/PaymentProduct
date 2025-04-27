0f83eebef019d4ddf83cbc10bdd474cde323fb0e
Vinicius Guimaraes da Cruz <0120857@trombini.local>
2025-04-27 20:11:42 -0300

    Codigo formatado

--- paymentService/index.js
+++ paymentService/index.js
@@ -1,65 +1,65 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const port = 3001;
+  const express = require('express');
+  const axios = require('axios');
+  const app = express();
+  const port = 3001;
 
-const payments = {};
+  const payments = {};
 
-app.use(express.json());
+  app.use(express.json());
 
-app.post('/charge', async (req, res) => {
-  try {
-    const { userId, amount } = req.body;
+  app.post('/charge', async (req, res) => {
+    try {
+      const { userId, amount } = req.body;
 
-    if (!userId || !amount) {
-      return res.status(400).json({ error: 'userId e amount são obrigatórios' });
+      if (!userId || !amount) {
+        return res.status(400).json({ error: 'userId e amount são obrigatórios' });
+      }
+      const response = await axios.get(`http://localhost:3001/products`);
+      const paymentId = Date.now();
+      
+      payments[paymentId] = { userId, amount, status: 'PENDING' };
+
+      console.log(`Processando pagamento de ${amount} para o usuário ${userId}`);
+
+      setTimeout(async () => {
+        payments[paymentId].status = 'COMPLETED';
+        console.log(`Pagamento de ${amount} concluído para o usuário ${userId}`);
+
+        try {
+          await axios.post('http://localhost:3001/payment-status', {
+            paymentId,
+            status: 'COMPLETED'
+          });
+          console.log('Notificação enviada para ProductService.');
+        } catch (error) {
+          console.error('Erro ao notificar ProductService:', error.message);
+        }
+
+      }, 3002);
+
+      res.status(201).json({ paymentId, status: 'PENDING' });
+    } catch (error) {
+      console.error('Erro ao processar pagamento:', error);
+      res.status(500).json({ error: 'Erro no servidor ao processar o pagamento' });
     }
-    const response = await axios.get(`http://localhost:3001/products`);
-    const paymentId = Date.now();
-    
-    payments[paymentId] = { userId, amount, status: 'PENDING' };
+  });
 
-    console.log(`Processando pagamento de ${amount} para o usuário ${userId}`);
+  app.get('/payment-status/:paymentId', (req, res) => {
+    try {
+      const { paymentId } = req.params;
+      const payment = payments[paymentId];
 
-    setTimeout(async () => {
-      payments[paymentId].status = 'COMPLETED';
-      console.log(`Pagamento de ${amount} concluído para o usuário ${userId}`);
-
-      try {
-        await axios.post('http://localhost:3001/payment-status', {
-          paymentId,
-          status: 'COMPLETED'
-        });
-        console.log('Notificação enviada para ProductService.');
-      } catch (error) {
-        console.error('Erro ao notificar ProductService:', error.message);
+      if (payment) {
+        return res.json(payment);
       }
 
-    }, 3002);
-
-    res.status(201).json({ paymentId, status: 'PENDING' });
-  } catch (error) {
-    console.error('Erro ao processar pagamento:', error);
-    res.status(500).json({ error: 'Erro no servidor ao processar o pagamento' });
-  }
-});
-
-app.get('/payment-status/:paymentId', (req, res) => {
-  try {
-    const { paymentId } = req.params;
-    const payment = payments[paymentId];
-
-    if (payment) {
-      return res.json(payment);
+      res.status(404).json({ error: 'Pagamento não encontrado' });
+    } catch (error) {
+      console.error('Erro ao consultar status do pagamento:', error);
+      res.status(500).json({ error: 'Erro no servidor ao consultar status do pagamento' });
     }
+  });
 
-    res.status(404).json({ error: 'Pagamento não encontrado' });
-  } catch (error) {
-    console.error('Erro ao consultar status do pagamento:', error);
-    res.status(500).json({ error: 'Erro no servidor ao consultar status do pagamento' });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`PaymentService rodando na porta ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`PaymentService rodando na porta ${port}`);
+  });
--- productService/index.js
+++ productService/index.js
@@ -6,28 +6,27 @@ const port = 3001;
 app.use(express.json());
 
 const products = [
-    {id: 1, name: 'Chocolate', price: 5},
-    {id: 2, name: 'Monster', price: 12},
-    {id: 3, name: 'Coxinha', price: 7}
+    { id: 1, name: 'Chocolate', price: 5 },
+    { id: 2, name: 'Monster', price: 12 },
+    { id: 3, name: 'Coxinha', price: 7 }
 ];
 
-app.get('/products', (req,res) => {
+app.get('/products', (req, res) => {
     res.json(products);
 })
 
-app.post('/payment-status', (req,res)=>{
+app.post('/payment-status', (req, res) => {
     const { paymentId, status } = req.body;
 
     console.log(`Recebido status de pagamento: ${paymentId} - ${status}`);
 
     if (status === 'COMPLETED') {
-        res.json({message:`Pagamento confirmado, seu pedido esta sendo processado.`});
-    }else{
-        res.json({message:`Pagamento não confirmado, tente novamente.`});
+        res.json({ message: `Pagamento confirmado, seu pedido esta sendo processado.` });
+    } else {
+        res.json({ message: `Pagamento não confirmado, tente novamente.` });
     }
 });
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`ProductService rodando na porta ${port}`);
 });
-
